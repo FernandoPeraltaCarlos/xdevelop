8bbd807c1cc2a25082f44325926415bed4516e51
FernandoPeraltaCarlos <fernando_peraltac@hotmail.com>
2023-10-02 17:45:16 -0600

    [root] Start

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+import million from 'million/compiler';
+import { defineConfig } from 'astro/config';
+
+import react from "@astrojs/react";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [react()],
+  vite: {
+    plugins: [million.vite({ mode: 'react', server: true, auto: true })]
+  }
+});
