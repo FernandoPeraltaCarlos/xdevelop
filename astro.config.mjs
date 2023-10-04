825237c43f23745e5b7d2fd3398cafe97a6e37e9
FernandoPeraltaCarlos <fernando_peraltac@hotmail.com>
2023-10-04 16:53:26 -0600

    [root] fix bugs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+  adapter: nodejs(),
   integrations: [react()],
   /*   vite: {
       plugins: [million.vite({ mode: 'react', server: true, auto: true })]
