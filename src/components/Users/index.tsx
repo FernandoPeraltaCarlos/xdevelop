e887fd9b5bb3fa8c6f5a720282ace93e0aed7dca
FernandoPeraltaCarlos <fernando_peraltac@hotmail.com>
2023-10-03 22:59:32 -0600

    [users] it's created the function of delete a user

--- src/components/Users/index.tsx
+++ src/components/Users/index.tsx
@@ -3,7 +3,12 @@ import style from './style.module.scss'
 
 import { get_all_users } from '../../API/users';
 
-function Card ({id, names, mail}){
+function Card ({id, names, mail, index, deleteUser}){
+
+    function deleteOfList(){
+        deleteUser(index)
+    }
+
     return(
         <li className={style.card}>
             <p>{id}</p>
@@ -12,6 +17,7 @@ function Card ({id, names, mail}){
                 <p>{mail}</p>
             </div>
             <a href="#">Ver mas</a>
+            <p onClick={deleteOfList}>X</p>
         </li>
     )
 }
@@ -22,6 +28,8 @@ function Users (){
     const [sortId, setSortId] = useState(true);
     const [sortEmail, setSortEmail] = useState(true);
 
+
+/* The `useEffect` hook is used to perform side effects in a functional component. In this case, it is used to fetch all users from an API and update the state variables `users` and `originalUsers` with the fetched data. */
     useEffect(()=>{
         async function getAllUsers (){
             const data = await get_all_users();
@@ -32,6 +40,8 @@ function Users (){
         getAllUsers();
     },[])
 
+
+/* The function `sortByID` sorts an array of users by their ID in ascending or descending order and updates the state with the sorted array. */
     function sortByID (){
         let usersCopy = [...users];
         if(sortId){
@@ -43,6 +53,7 @@ function Users (){
         setUsers(usersCopy)
     }
 
+/*The function `sortByEmail` sorts an array of users by their email in ascending or descending order, toggling the sorting order each time it is called.*/
     function sortByEmail (){
         let usersCopy = [...users];
         if(sortEmail){
@@ -66,6 +77,13 @@ function Users (){
         setUsers(usersCopy)
     }
 
+/**The deleteUser function removes a user from an array of users and updates the state with the updated array.@param index - The index parameter represents the index of the user that needs to be deleted from the users array.*/
+    function deleteUser (index){
+        let userCopy = [...users];
+        userCopy.splice(index,1)
+        setUsers(userCopy)
+    }
+
     return(
         <section className={style.users}>
             <div className={style.orderList}>
@@ -76,8 +94,9 @@ function Users (){
                 {
                     users.length === 0 ?
                     <li>Cargando....</li>:
-                    users.map(user=>(
-                        <Card key={user.id} id={user.id} names={user.name} mail={user.email}/>
+                    users.map((user,index)=>(
+                        <Card key={user.id} 
+                            deleteUser={deleteUser} index={index} id={user.id} names={user.name} mail={user.email}/>
                     ))
                 }
             </ul>
