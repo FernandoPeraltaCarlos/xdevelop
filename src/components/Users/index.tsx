1cfc4c311999790ab5f84721f5e04fe20e19eef5
FernandoPeraltaCarlos <fernando_peraltac@hotmail.com>
2023-10-03 21:11:49 -0600

    [users] it's created the sortById function

--- src/components/Users/index.tsx
+++ src/components/Users/index.tsx
@@ -18,18 +18,36 @@ function Card ({id, names, mail}){
 
 function Users (){
     const [users, setUsers] = useState([]);
+    const [originalUsers, setOriginalUsers] = useState([]);
+    const [sortId, setSortId] = useState(true);
 
     useEffect(()=>{
         async function getAllUsers (){
             const data = await get_all_users();
             setUsers([...data])
+            setOriginalUsers([...data]);
         }
 
         getAllUsers();
     },[])
 
+    function sortByID (){
+        let usersCopy = [...users];
+        if(sortId){
+            usersCopy.sort((a,b) => {return b.id - a.id})
+        }else{
+            usersCopy.sort((a,b) => {return a.id - b.id})
+        }
+        setSortId(!sortId);
+        setUsers(usersCopy)
+    }
+
     return(
         <section className={style.users}>
+            <div className={style.orderList}>
+                <p onClick={sortByID} >Ordenar por Id</p>
+                <p>Ordenar por Correo</p>
+            </div>
             <ul>
                 {
                     users.length === 0 ?
