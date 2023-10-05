493644ce5cfded72995c7469123ffbcf298cb897
FernandoPeraltaCarlos <fernando_peraltac@hotmail.com>
2023-10-05 13:58:00 -0600

    [session] it´s chaged a diretective in script

---
+++ .vercel/output/static/_astro/hoisted.721a4833.js
@@ -0,0 +1,1 @@
+import"./hoisted.bd7fdfe4.js";const o=localStorage.getItem("token");o||(window.location.href="/");function e(){localStorage.removeItem("token"),location.reload()}let t=document.querySelector("#close");t.onclick=function(){e()};
