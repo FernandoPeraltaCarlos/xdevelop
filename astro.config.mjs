0e02ebd5e7bec9152ee255fb27f855b9dce1756d
FernandoPeraltaCarlos <fernando_peraltac@hotmail.com>
2023-10-05 13:46:49 -0600

    [root] add vercel deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,15 @@
 import million from 'million/compiler';
 import { defineConfig } from 'astro/config';
 import nodejs from '@astrojs/node';
-
 import react from "@astrojs/react";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: nodejs({
-    mode: 'standalone',
-  }),
-  integrations: [react()],
+  adapter: vercel(),
+  integrations: [react()]
   /*   vite: {
       plugins: [million.vite({ mode: 'react', server: true, auto: true })]
     } */
